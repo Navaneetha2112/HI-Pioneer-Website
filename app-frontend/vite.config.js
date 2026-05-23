629030c40ed2f95bdc316896607871c0a9d939db
Your Name <you@example.com>
2026-05-24 00:44:49 +0530

    nava

--- app-frontend/vite.config.js
+++ app-frontend/vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from "vite";
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: '/HI-Pioneer-Website/',
     plugins: [react()],
     build: {
         outDir: './build',
